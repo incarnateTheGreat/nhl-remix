c2b34807f6fc8504e5ff7d145da93bdcaf7f0341
Garry T <incarnate@rogers.com>
2024-11-16 01:00:07 -0500

    Reverted.

--- app/sse/create-game-event-stream.server.ts
+++ app/sse/create-game-event-stream.server.ts
@@ -2,7 +2,7 @@ import { eventStream } from "remix-utils/sse/server";
 import { Game } from "types/types";
 
 import getGameData from "~/api/getGameData";
-import { Timer } from "~/utils";
+import { isGameActive, isGameComplete, isPreGame, Timer } from "~/utils";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
@@ -14,22 +14,22 @@ export function createGameEventStream(request: Request, gameId: string) {
       const gameData: Game = await getGameData(gameId);
       const gameDataToString = JSON.stringify(gameData);
 
-      // const {
-      //   gameState,
-      //   clock: { inIntermission },
-      // } = gameData;
+      const {
+        gameState,
+        clock: { inIntermission },
+      } = gameData;
 
-      // if (isPreGame(gameState) || inIntermission) {
-      //   timerToUse.start(() => {
-      //     run();
-      //   }, 60000);
-      // } else if (isGameActive(gameState) && !inIntermission) {
-      //   timerToUse.start(() => {
-      //     run();
-      //   }, 15000);
-      // } else if (timerToUse.running && isGameComplete(gameState)) {
-      //   timerToUse.stop();
-      // }
+      if (isPreGame(gameState) || inIntermission) {
+        timerToUse.start(() => {
+          run();
+        }, 60000);
+      } else if (isGameActive(gameState) && !inIntermission) {
+        timerToUse.start(() => {
+          run();
+        }, 15000);
+      } else if (timerToUse.running && isGameComplete(gameState)) {
+        timerToUse.stop();
+      }
 
       send({
         data: gameDataToString,
