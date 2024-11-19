4ba25d5f8b8fc009b823d7a0b3f42ea521a6591b
Garry T <incarnate@rogers.com>
2024-11-19 00:21:06 -0500

    Set condition in the Emitter Loader function to not load the Event Stream if the Game is complete.

--- app/routes/emitter.$gameId.tsx
+++ app/routes/emitter.$gameId.tsx
@@ -8,21 +8,23 @@ import { isGameActive, isGameComplete, isPreGame, Timer } from "~/utils";
 // @ts-ignore
 const timerToUse = new Timer();
 
-export const loader: LoaderFunction = ({ request, params }) => {
+export const loader: LoaderFunction = async ({ request, params }) => {
   const { gameId = "" } = params;
+
+  const gameData = await getGameData(gameId);
+
+  const { gameState } = gameData;
+
+  if (isGameComplete(gameState)) {
+    return null;
+  }
 
   // Return the EventStream from your route loader
   return new EventStream(request, (send) => {
-    // In the init function, setup your SSE Event source
-    // This can be any asynchronous data source, that will send
-    // events to the client periodically
-
-    // Here we will just use a `setInterval`
-
-    const init = async () => {
-      const gameData = await getGameData(gameId);
-      send(JSON.stringify(gameData));
-    };
+    // const init = async () => {
+    //   const gameData = await getGameData(gameId);
+    //   send(JSON.stringify(gameData));
+    // };
 
     const run = async () => {
       const gameData = await getGameData(gameId);
@@ -36,12 +38,12 @@ export const loader: LoaderFunction = ({ request, params }) => {
         timerToUse.start(() => {
           send(JSON.stringify(gameData));
           run();
-        }, 5000);
+        }, 5000); // Change back to 60000
       } else if (isGameActive(gameState) && !inIntermission) {
         timerToUse.start(() => {
           send(JSON.stringify(gameData));
           run();
-        }, 45000);
+        }, 15000);
       }
 
       if (timerToUse.running && isGameComplete(gameState)) {
@@ -49,23 +51,10 @@ export const loader: LoaderFunction = ({ request, params }) => {
       }
     };
 
-    init();
+    // init();
     run();
 
-    // const interval = setInterval(async () => {
-    //   const gameData = await getGameData(gameId);
-
-    //   // You can send events to the client via the `send` function
-    //   send(JSON.stringify(gameData));
-
-    //   //   // if you to send different events on multiple channels you can
-    //   //   // specify an eventKey in the options
-    //   //   send(JSON.stringify({ hello: "world" }), { eventKey: "channelExample" });
-    // }, 5000);
-
     return () => {
-      // Return a cleanup function
-      // clearInterval(interval);
       timerToUse.stop();
     };
   });
