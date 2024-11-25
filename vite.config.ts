0023f38736432ec43a8c83bea2754f84dc3d2e92
Garry T <incarnate@rogers.com>
2024-11-24 23:52:01 -0500

    Reversion of Fly.io content

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
-import { vercelPreset } from "@vercel/remix/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -10,13 +9,9 @@ export default defineConfig({
   plugins: [
     remix({
       ignoredRouteFiles: ["**/*.css"],
-      presets: [vercelPreset()],
     }),
     tsconfigPaths(),
   ],
-  ssr: {
-    noExternal: ["tailwind-merge"],
-  },
   server: {
     host: true,
     port: 5175,
