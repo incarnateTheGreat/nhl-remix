76baa4f1858a555bcfd601ed9817a4b355befd07
Garry T <incarnate@rogers.com>
2025-04-29 19:13:45 -0400

    Added nullish coalescing for Scoring data.

--- app/routes/game.$gameId/components/Scoring/index.tsx
+++ app/routes/game.$gameId/components/Scoring/index.tsx
@@ -16,7 +16,7 @@ export default function Scoring() {
   return (
     <div className="default_border flex flex-1 flex-col p-2">
       <h2 className="heading">Scoring</h2>
-      {scoringNoShootout.map((period) => {
+      {scoringNoShootout?.map((period) => {
         return (
           <div key={period.periodDescriptor.number} className="mb-4">
             <h2 className="mb-2 border-b border-gray-700 font-bold">
