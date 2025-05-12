f5ae3a16994a27d260093ec709dbb78239e60b21
Garry T <incarnate@rogers.com>
2025-05-12 11:05:02 -0400

    Added conditional checks for Highlights.

--- app/api/getGameData.ts
+++ app/api/getGameData.ts
@@ -21,25 +21,32 @@ export default async function getGameData(gameId: string) {
     const rightRail: Summary = await rightRailResponse.json();
 
     if (rightRail?.gameVideo) {
-      const { data: threeMinRecapData } = await brightcoveApi.get(
-        `/playback/v1/accounts/6415718365001/videos/${rightRail.gameVideo.threeMinRecap}`,
-      );
+      if (rightRail?.gameVideo?.threeMinRecap) {
+        const { data: threeMinRecapData } = await brightcoveApi.get(
+          `/playback/v1/accounts/6415718365001/videos/${rightRail.gameVideo.threeMinRecap}`,
+        );
 
-      rightRail.gameVideo.threeMinRecapVideoObject = {
-        poster: threeMinRecapData.poster,
-        videoUrl:
-          threeMinRecapData.sources[threeMinRecapData.sources.length - 1].src,
-      };
+        rightRail.gameVideo.threeMinRecapVideoObject = {
+          poster: threeMinRecapData.poster,
+          videoUrl:
+            threeMinRecapData.sources[threeMinRecapData.sources.length - 1].src,
+        };
+      }
 
-      const { data: condensedGameData } = await brightcoveApi.get(
-        `/playback/v1/accounts/6415718365001/videos/${rightRail.gameVideo.condensedGame}`,
-      );
+      if (rightRail?.gameVideo?.condensedGame) {
+        const { data: condensedGameData } = await brightcoveApi.get(
+          `/playback/v1/accounts/6415718365001/videos/${rightRail.gameVideo.condensedGame}`,
+        );
 
-      rightRail.gameVideo.condensedGameVideoObject = {
-        poster: condensedGameData.poster,
-        videoUrl:
-          condensedGameData.sources[condensedGameData.sources.length - 1].src,
-      };
+        if (condensedGameData) {
+          rightRail.gameVideo.condensedGameVideoObject = {
+            poster: condensedGameData.poster,
+            videoUrl:
+              condensedGameData.sources[condensedGameData.sources.length - 1]
+                .src,
+          };
+        }
+      }
     }
 
     const data = deepMerge(gameData, boxscoreData, rightRail) as Game & Summary;
